a2da6747e2d6909edcebd047bcf36325d3aadcf3
garcajuDev2019 <garcajuvueling@gmail.com>
2019-02-10 12:47:40 +0100

    Agregar archivos de proyecto.

---
+++ using_dispose/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace using_dispose
+{
+    internal class Persona : IDisposable
+    {
+        private int age;
+
+        public Persona(int edad)
+        {
+            this.age = edad;
+        }
+
+        public override string ToString() => string.Format($"La edad de esta persona es de {age} años");
+
+        public void Dispose() => Console.WriteLine("Se ha llamado a .Dispose(), aquí se libera el recurso no administrado");
+    }
+}
---
+++ using_dispose/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace using_dispose
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WithDisposeMethod();
+            WithUsingMethod(); 
+        }
+
+        private static void WithDisposeMethod()
+        {
+            Persona persona1 = new Persona(27);
+
+            Console.WriteLine(persona1);
+            if (persona1 is IDisposable) { persona1.Dispose(); }
+        }
+
+        private static void WithUsingMethod()
+        {
+            using(Persona persona2 = new Persona(37))
+            {
+                Console.WriteLine(persona2);
+            }
+        }
+    }
+}
